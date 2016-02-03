987473164fc0c5050c801609d8c9855363595714
Brayden Traas <btraas@gmail.com>
2016-02-02 16:10:59 -0800

    Carriers only startingfleets, new game mode, race fix for single player

---
+++ scripts/rules/deathmatchcata/unitcaps/unlimited.lua
@@ -0,0 +1,22 @@
+-- Yelow13 - "Unlimited" sets all to 99999
+
+UC_Family("Fighter", 99999);
+UC_Family("Fighter_hw1", 99999);
+UC_Family("Corvette", 99999);
+UC_Family("Corvette_hw1", 99999);
+UC_Family("Frigate", 99999);
+UC_Family("Capital", 99999);
+UC_Family("Utility", 99999);
+UC_Family("Resource", 99999);
+UC_Family("NonCombat", 99999);
+UC_ShipType("Research", 1);
+UC_ShipType("Research1", 1);
+UC_ShipType("Research2", 1);
+UC_ShipType("Research3", 1);
+UC_ShipType("Research4", 1);
+UC_ShipType("Research5", 1);
+UC_Family("Platform", 99999);
+UC_Family("Mothership", 99999);
+UC_Family("SinglePlayerMisc", 99999);
+
+
---
+++ scripts/startingfleets/somtaaw00carriersonly.lua
@@ -0,0 +1,28 @@
+PersistantData = {
+  StrikeGroups = {
+  },
+  Squadrons = {
+    {
+      type = "Hgn_Smt_Carrier",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+			number = 1,
+    },
+    {
+      type = "Hgn_Smt_Worker",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 6,
+    },    
+  },
+	Research = {
+	},
+}
