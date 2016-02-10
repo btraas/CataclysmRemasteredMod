514a09952fd0d808141a9739e6dd1665544d95db
Brayden Traas <btraas@gmail.com>
2016-02-09 23:42:27 -0800

    Updated config file, temporarily removed SP from main menu

--- ui/newui/main/newmainmenu.lua
+++ ui/newui/main/newmainmenu.lua
@@ -138,16 +138,16 @@ NewMainMenu = {
 					type = "Frame",
 					size = {200, 20},
 				},		
---	NO ROOM!	NewMenuButton("btnTutorial",		"TRAINING",	nil,	0,	LAYOUT_MENU_BUTTONS,	"FEButtonStyleCataMain",	nil),
---				{
---					type = "Frame",
---					size = {200, 4},
---				},	
-				NewMenuButton("btnExtra",			"SINGLE PLAYER (INCOMPLETE)",	nil,	0,	LAYOUT_MENU_BUTTONS,	"FEButtonStyleCataMain",	nil),
+				NewMenuButton("btnTutorial",		"TRAINING",	nil,	0,	LAYOUT_MENU_BUTTONS,	"FEButtonStyleCataMain",	nil),
 				{
 					type = "Frame",
 					size = {200, 4},
-				},
+				},	
+--				NewMenuButton("btnExtra",			"SINGLE PLAYER (INCOMPLETE)",	nil,	0,	LAYOUT_MENU_BUTTONS,	"FEButtonStyleCataMain",	nil),
+--				{
+--					type = "Frame",
+--					size = {200, 4},
+--				},
 				NewMenuButton("btnPlayerVsCPU",		"SKIRMISH VS CPU",	nil,	0,	LAYOUT_MENU_BUTTONS,	"FEButtonStyleCataMain",	nil),
 				{
 					type = "Frame",
