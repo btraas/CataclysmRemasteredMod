7d2bb43861d6c2d2161adcbc31314c93c5b43e88
Brayden Traas <btraas@gmail.com>
2016-03-08 19:31:02 -0800

    Acolyte & superacolyte animations, Somtaaw dreadnought ion update

--- scripts/startingfleets/somtaaw00.lua
+++ scripts/startingfleets/somtaaw00.lua
@@ -3,7 +3,7 @@ PersistantData = {
   },
   Squadrons = {
     {
-      type = "Hgn_smt_ms",
+      type = "Hgn_Smt_ms",
       subsystems = {
       },
       shiphold = {
@@ -11,17 +11,18 @@ PersistantData = {
       name = "",
       size = 1,
     },
-    {
-      type = "Hgn_smt_carrier",
+	{
+      type = "Hgn_Smt_Processor",
       subsystems = {
       },
       shiphold = {
       },
       name = "",
       size = 1,
+      number = 1,
     },
     {
-      type = "Hgn_smt_worker",
+      type = "Hgn_Smt_mimic",
       subsystems = {
       },
       shiphold = {
@@ -29,6 +30,146 @@ PersistantData = {
       name = "",
       size = 1,
       number = 6,
+    },
+	{
+      type = "Hgn_Smt_acv",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 12,
+    },
+	{
+      type = "Hgn_Smt_leech",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 12,
+    },
+	{
+      type = "Bst_fieldfrigate",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+	{
+      type = "Bst_ionarrayfrigate",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+	{
+      type = "Bst_carrier",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+	{
+      type = "Bst_ioncannonfrigate",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+	{
+      type = "Hgn_Smt_dreadnought",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+	{
+      type = "Tai_HeavyCruiser",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+	{
+      type = "Bst_attackbomber",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+	{
+      type = "Bst_fighter",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+	{
+      type = "Bst_cloakedfighter",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+	{
+      type = "Bst_interceptor",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1,
+    },
+    {
+      type = "Hgn_Smt_Carrier",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1, 
+    },
+	{
+      type = "Hgn_Smt_Hivefrigate",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1, 
     },
   },
 	Research = {
---
+++ scripts/startingfleets/somtaw00.lua
@@ -0,0 +1,36 @@
+PersistantData = {
+  StrikeGroups = {
+  },
+  Squadrons = {
+    {
+      type = "Hgn_smt_ms",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+    },
+    {
+      type = "Hgn_smt_carrier",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+    },
+    {
+      type = "Hgn_smt_worker",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 6,
+    },
+  },
+	Research = {
+	},
+}
